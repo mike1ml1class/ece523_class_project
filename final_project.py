8e37ad3908e7cbafa88667a083130b5d7aea0204
mike1ml1class <38625747+mike1ml1class@users.noreply.github.com>
2018-04-22 09:25:17 -0700

    some boilerplate

--- final_project.py
+++ final_project.py
@@ -1,3 +1,11 @@
 # Final project scripts
 # cd C:\Users\Michael\Desktop\ECE523\project\ece523_class_project
+import numpy as np
+import matplotlib.pyplot as plt
+from helpers import *
 
+# Load the titanic training and testing data
+#dat_tr   = np.loadtxt('train.csv',delimiter=',')
+#dat_test = np.loadtxt('test.csv',delimiter=',')
+#X_tr     = dat_tr[:,0:-2]
+#Y_tr     = dat_tr[:,-1]
