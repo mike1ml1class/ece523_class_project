f3ce02952538efe449322df24ed27d93cf752e5a
mike1ml1class <38625747+mike1ml1class@users.noreply.github.com>
2018-04-22 13:32:47 -0700

    changing comments around

--- final_project.py
+++ final_project.py
@@ -18,14 +18,15 @@ data_test.info()
 # Age, Cabin, and Embarked have missing data
 # Cabin has too much missing data to be useful, so that will be dropped
 # Take average of age and replace missing age values with average
+# Embared will be changed to most common occurance
+# Change male and female strings to 0 or 1
+# Changed point of depature strings to 0, 1, or 2. 
 
 data_train = data_train.drop(['Cabin'], axis=1)
 data_test  = data_test.drop(['Cabin'], axis=1)
 data_train.info()
 combined_data = [data_train,data_test]
-# - Change male and female strings to 0 or 1
-# - Changed point of depature strings to 0, 1, or 2. Fill in missing
-#   data with most common occurance
+
 most_common_port = data_train.Embarked.dropna().mode()[0]
 average_age      = data_train.Age.dropna().mean()
 for dataset in combined_data:
@@ -33,8 +34,6 @@ for dataset in combined_data:
     dataset['Sex']      = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
     dataset['Embarked'] = dataset['Embarked'].fillna(most_common_port)
     dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
-
-
 
 data_train.head()
 data_test.head()
