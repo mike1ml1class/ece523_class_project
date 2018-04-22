adda989922bbaf6a9ec654eaa05a94e8c7a206b7
mike1ml1class <38625747+mike1ml1class@users.noreply.github.com>
2018-04-22 12:54:19 -0700

    more data cleaning

--- final_project.py
+++ final_project.py
@@ -9,9 +9,20 @@ from helpers import *
 data_train = pd.read_csv('train.csv')
 data_test  = pd.read_csv('test.csv')
 
-# Clean the data: Change male and female strings to 0 or 1
-data_train['Sex'] = data_train['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
-data_test['Sex']  = data_test['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+combined_data = [data_train,data_test]
+
+# Clean the data: 
+# - Change male and female strings to 0 or 1
+# - Changed point of depature strings to 0, 1, or 2. Fill in missing
+#   data with most common occurance
+
+most_common_port = data_train.Embarked.dropna().mode()[0]
+for dataset in combined_data:
+    dataset['Sex']      = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+    dataset['Embarked'] = dataset['Embarked'].fillna(most_common_port)
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+
 
 data_train.head()
 data_test.head()
