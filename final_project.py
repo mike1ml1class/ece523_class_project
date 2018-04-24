f067f2664d6b7fe04bf4f7a67993efbc78e0817f
mike1ml1class <38625747+mike1ml1class@users.noreply.github.com>
2018-04-23 18:51:04 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/mike1ml1class/ece523_class_project""
    This reverts commit d1eca8c22ad77bef372a0b610a62b5f93e2a5f51.

--- final_project.py
+++ final_project.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 from helpers import *
 from sklearn.model_selection import cross_val_score
 from sklearn import svm
+<<<<<<< HEAD
 from sklearn.decomposition import PCA
+=======
+import sys
+>>>>>>> 845d21325f3b91bd618909eca5b7ac4626c966a7
 
 GEN_OUTPUT = False
 
@@ -17,37 +21,97 @@ data_test  = pd.read_csv('test.csv')
 data_train_orig = data_train
 data_test_orig  = data_test
 
-data_train.info()
-print('_________________________________')
-print('_________________________________')
-data_test.info()
+print('\nTraining Data:')
+print( data_train.info() )
 
-# Clean the data: 
+print('\nTesting Data:')
+print( data_test.info() )
+
+print('\nDescribe Training:')
+print( data_train.describe() )
+
+
+
+# Clean the data:
 # Age, Cabin, and Embarked have missing data
 # Cabin has too much missing data to be useful, so that will be dropped
 # PassengerID, Name, and Ticket will also be dropped
 # Take average of age and replace missing age values with average
 # Embared will be changed to most common occurance
 # Change male and female strings to 0 or 1
-# Changed point of depature strings to 0, 1, or 2. 
+# Changed point of depature strings to 0, 1, or 2.
 # This will leave us with numeric data to use in ML models
 # Test data has a missing value for Fare, this will be replaced
 # by the average fare
 
-# Drop irrelevent data
-data_train = data_train.drop(['Cabin'], axis=1)
-data_test  =  data_test.drop(['Cabin'], axis=1)
+# Do a bit of analysis by isolating features
+print('\n')
+out = data_train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(out)
+
+print('\n')
+out = data_train[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(out)
+
+print('\n')
+out = data_train[['Cabin', 'Survived']].groupby(['Cabin'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(out)
+
+print('\n')
+out = data_train[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(out)
+
+print('\n')
+out = data_train[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(out)
+
+
+
+f,ax = plt.subplots(1,2,figsize=(8,4))
+#ax = data_train.hist(column='Age',by='Survived',sharey=True,xrot=45,bins=20,ec='k')
+data_train.hist(ax=ax,column='Age',by='Survived',rot=0,bins=20,ec='k')
+ax[0].set_xlabel('Age')
+ax[1].set_xlabel('Age')
+ax[0].grid(True)
+ax[1].grid(True)
+ax[0].set_title('Survived = 0')
+ax[1].set_title('Survived = 1')
+plt.tight_layout()
+f.savefig('Age_hist')
+
+f,ax = plt.subplots(3,2,figsize=(8,8))
+#data_train.hist(ax=ax,column='Age',by=['Pclass','Survived'],sharey=True,sharex=True,xrot=45,bins=20,ec='k')
+data_train.hist(ax=ax,column='Age',by=['Pclass','Survived'],xrot=45,bins=20,ec='k')
+ax[2,0].set_xlabel('Age')
+ax[0,0].set_title('Pclass = 1,Survived = 0')
+ax[0,1].set_title('Pclass = 1,Survived = 1')
+ax[1,0].set_title('Pclass = 2,Survived = 0')
+ax[1,1].set_title('Pclass = 2,Survived = 1')
+ax[2,0].set_title('Pclass = 3,Survived = 0')
+ax[2,1].set_title('Pclass = 3,Survived = 1')
+plt.tight_layout()
+f.savefig('Pclass_hist')
+
+
+'''
+
+# Drop irrelevent features that should not effect survival
+data_train = data_train.drop(['Cabin']      , axis=1)
 data_train = data_train.drop(['PassengerId'], axis=1)
+data_train = data_train.drop(['Ticket']     , axis=1)
+data_train = data_train.drop(['Name']       , axis=1)
+
+data_test  =  data_test.drop(['Cabin']      , axis=1)
 data_test  =  data_test.drop(['PassengerId'], axis=1)
-data_train = data_train.drop(['Ticket'], axis=1)
-data_test  =  data_test.drop(['Ticket'], axis=1)
-data_train = data_train.drop(['Name'], axis=1)
-data_test  =  data_test.drop(['Name'], axis=1)
+data_test  =  data_test.drop(['Ticket']     , axis=1)
+data_test  =  data_test.drop(['Name']       , axis=1)
+
 
 data_train.info()
 combined_data = [data_train,data_test]
 
-# Further clean the data 
+
+# Further clean the data
 most_common_port = data_train.Embarked.dropna().mode()[0]
 average_age      = data_train.Age.dropna().mean()
 average_fare     = data_train.Fare.dropna().mean()
@@ -81,11 +145,11 @@ for i,clf in enumerate(classifiers):
     scores = cross_val_score(clf, X_train, Y_train, cv=5)
     print("%s %.2f" % (class_names[i],scores.mean()*100))
     classifier_scores.append(scores.mean()*100)
-    
+
     # Train with all the data?
     clf.fit(X_train,Y_train)
-    
-    # Predict the Results with actual test data and generate CSV that 
+
+    # Predict the Results with actual test data and generate CSV that
     # Kaggle needs to actually score
     results = clf.predict(X_test)
 
@@ -93,6 +157,8 @@ submission = pd.DataFrame({
         "PassengerId": data_test_orig["PassengerId"],
         "Survived": results
     })
-    
+
 if GEN_OUTPUT:
     submission.to_csv('./output/submission.csv', index=False)
+
+'''
