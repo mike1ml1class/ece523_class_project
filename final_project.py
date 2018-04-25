38dec338a1b13c5de637bb3af7c6e24e48d6c186
mike1ml1class <38625747+mike1ml1class@users.noreply.github.com>
2018-04-24 23:13:57 -0700

    hooking up classifiers and commenting out other stuff in loop.  need to modify classifier functions to return score for meta analysis and return predicted results for official test scoring

--- final_project.py
+++ final_project.py
@@ -8,13 +8,19 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_score
-from sklearn import svm
 
 # Import our modules
 import helpers as hp
-import pca
-import dnn
 import titanic
+import knn as knn
+import logistic_regression as lr
+import random_forest as rf
+import svm as svm
+import decision_tree as dt
+import pca as pca
+import dnn as dnn
+
+
 
 # Setup and Config
 GEN_OUTPUT    = True
@@ -28,7 +34,6 @@ data_test  = pd.read_csv('test.csv')
 
 # Visualize data and correlations to survival
 if VISUALIZE:
-
     titanic.visualize_data(data_train,data_test)
 
 # Convert categorical data to numerical and fill in missing values
@@ -40,16 +45,21 @@ df_te = data[1]                  # Testing data
 
 
 if PERFORM_CLASS:
-
     x_tr = df_tr.drop("Survived", axis=1)
     y_tr = df_tr["Survived"]
     x_te = df_te
 
-
     # Loop through classifiers and perform k-fold cross validation
-    classifiers = [svm.SVC(kernel='linear', C=1)]
-    class_names = ['SVM']
-
+    #classifiers = [svm.SVC(kernel='linear', C=1)]
+    
+    
+    classifiers = [svm.analysis,
+                   lr.analysis,
+                   rf.analysis,
+                   knn.analysis,
+                   dt.analysis]
+                   
+    class_names = ['SVM','LogisticRegression','RandomForest','KNN','DecsionTree']
 
     # Create Reduced Feature Data using PCA
     # -Using three components to reduce features.  Will plot
@@ -61,26 +71,28 @@ if PERFORM_CLASS:
     #  sophisticated techniques will be needed to get a decent
     #  score
 
-    pca.analysis(x_tr,y_tr)
+    #pca.analysis(x_tr,y_tr)
 
     classifier_scores = []
     print("Classifier k-fold score")
-    for i,clf in enumerate(classifiers):
-        scores = cross_val_score(clf, x_tr, y_tr, cv=5)
-        print("%s %.2f" % (class_names[i],scores.mean()*100))
-        classifier_scores.append(scores.mean()*100)
+    for i,my_clf in enumerate(classifiers):
+        #scores = cross_val_score(clf, x_tr, y_tr, cv=5)
+        #print("%s %.2f" % (class_names[i],scores.mean()*100))
+        #classifier_scores.append(scores.mean()*100)
+        
+        my_clf(x_tr,y_tr)
 
         # Train with all the data?
-        clf.fit(x_tr,y_tr)
+        #clf.fit(x_tr,y_tr)
 
         # Predict the Results with actual test data and generate CSV that
         # Kaggle needs to actually score
-        y_pred = clf.predict(x_te)
+        #y_pred = clf.predict(x_te)
 
     if GEN_OUTPUT:
-        file_str = './output/' + class_names[i] + '_' + 'submission.csv'
-        titanic.create_submission(data_test,y_pred,file_str)
+        print('Generating Output...')
+        #file_str = './output/' + class_names[i] + '_' + 'submission.csv'
+        #titanic.create_submission(data_test,y_pred,file_str)
 
 if NEURAL_NET:
-
     dnn.analysis()
